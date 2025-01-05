84f49c03ea931c861c9a29e5ff6f4d4267495d3e
Or Levy <olevy111@gmail.com>
2025-01-05 11:58:10 +0200

    LAAAAASSST

--- Network.java
+++ Network.java
@@ -125,20 +125,14 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-        StringBuilder ans = new StringBuilder("Network:\n");
+        StringBuilder ans = new StringBuilder("Network:");
         for (int i = 0; i < userCount; i++) {
-            ans.append(users[i].getName()).append(" ->");
-            if (users[i].getfCount() > 0) {
-                ans.append(" ");
-            }
+            ans.append("\n").append(users[i].getName()).append(" ->");
             for (int j = 0; j < users[i].getfCount(); j++) {
-                ans.append(users[i].getfFollows()[j]);
-                if (j < users[i].getfCount() - 1) {
-                    ans.append(" ");
-                }
+                ans.append(" ").append(users[i].getfFollows()[j]);
             }
-            ans.append("\n");
+            ans.append(" "); 
         }
-        return ans.toString();
+        return ans.toString().trim();
     }
 }
